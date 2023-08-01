7826036158e9f5f80f64ebe4d772697e9d08e442
jon <neoeox@naver.com>
2023-08-01 09:50:38 +0900

    mod pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,31 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import os
+print(os.getcwd())
+name = ''
+shares = 0
+price = 0
+total_cost = 0
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    headers = next(f).split(',')
+    print(headers)
+    for line in f:
+        row = line.split(',')
+        name = row[0]
+        shares = int(row[1])
+        price = float(row[2])
+        total_cost = total_cost + (shares * price)
+    print('total_cost=', total_cost)
+
+
+#
+import gzip
+with gzip.open('Work/Data/portfolio.csv.gz', 'rt') as f:
+    for line in f:
+        print(line, end='')
+
+
+
+
