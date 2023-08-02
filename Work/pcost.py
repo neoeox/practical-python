537b4bc7a1a4f1b19f158af6023647726019cba0
jon <neoeox@naver.com>
2023-08-02 14:33:03 +0900

    mod pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -27,5 +27,57 @@ with gzip.open('Work/Data/portfolio.csv.gz', 'rt') as f:
         print(line, end='')
 
 
+def portfolio_cost(filename):
+    name = ''
+    shares = 0
+    price = 0
+    total_cost = 0
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        print(headers)
+        for line in f:
+            row = line.split(',')
+            name = row[0]
+            shares = int(row[1])
+            price = float(row[2])
+            total_cost = total_cost + (shares * price)
+    return total_cost
+
+cost = portfolio_cost('Work/Data/portfolio.csv')
+print('Total cost=', cost)
+
+
+#
+import sys
+
+def portfolio_cost(filename):
+    name = ''
+    shares = 0
+    price = 0
+    total_cost = 0
+    with open(filename, 'rt') as f:
+        headers = next(f).split(',')
+        print(headers)
+        for line in f:
+            row = line.split(',')
+            name = row[0]
+            shares = int(row[1])
+            price = float(row[2])
+            total_cost = total_cost + (shares * price)
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Work/Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost=', cost)
+
+
+
+
+
+
 
 
